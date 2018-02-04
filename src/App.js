62120e38775138da7d92ee248c12bdf009b24bc6
Tingxuan Zhang <zhangtx93@gmail.com>
2018-02-04 23:45:07 +1100

    split components

--- src/App.js
+++ src/App.js
@@ -48,39 +48,86 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        
-          <div className="App">
-          <form>
-            <input 
-              type="text" 
-              value={searchTerm}
-              onChange={this.onSearchChange}
-            />
-          </form>
-          {
-            list.filter(isSearched(searchTerm)).map(item => {
-              return (
-                <div key={item.objectID}>
-                  <span>
-                    <a href={item.url}>{item.title}</a>
-                  </span>
-                  <span>{item.author}</span>
-                  <span>{item.num_comments}</span>
-                  <span>{item.points}</span>
-                  <span>
-                    <button 
-                      onClick={() =>this.onDismiss(item.objectID)}
-                      type="button"
-                    >
-                      Dismiss
-                    </button>
-                  </span>
-                </div>
-              );
-            })
-          }
-          </div>
+        <Search 
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        >
+          Search
+        </Search>
+        <Table 
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
+    );
+  }
+}
+
+class Search extends React.Component {
+  render() {
+    const { value, onChange, children } = this.props;
+    return(
+      <form>
+        {children}
+        <input 
+          type="text" 
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    );
+  }
+}
+
+class Table extends React.Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+
+    return(
+      <div>
+        {
+          list.filter(isSearched(pattern)).map(item => {
+            return (
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <span>
+                  <Button 
+                    onClick={() =>onDismiss(item.objectID)}
+                  >
+                    Dismiss
+                  </Button>
+                </span>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
+
+class Button extends React.Component {
+  render() {
+    const {
+      onClick,
+      className = '',
+      children,
+    } = this.props;
+
+    return(
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
     );
   }
 }
